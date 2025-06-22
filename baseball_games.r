60608ab943c53b34f60ac2395c4870edf6a682ad
Anthony Giacalone <agiacalone@gmail.com>
2025-06-22 12:12:17 -0700

    fix null value breaking boxscore

--- baseball_games.r
+++ baseball_games.r
@@ -182,9 +182,15 @@ make_box_score <- function(linescore) {
   home_runs_by_inning <- as.character(linescore$home_runs)
   away_team <- unique(linescore$away_team_name)
   home_team <- unique(linescore$home_team_name)
-  totals <- linescore %>% filter(num == max(num))
-  away_runs_total <- as.character(sum(as.numeric(linescore$away_runs)))
-  home_runs_total <- as.character(sum(as.numeric(linescore$home_runs)))
+  # Helper for last non-NA value
+  last_non_na <- function(x) tail(x[!is.na(x)], 1)
+  # Totals
+  away_runs_total <- as.character(sum(as.numeric(linescore$away_runs), na.rm = TRUE))
+  home_runs_total <- as.character(sum(as.numeric(linescore$home_runs), na.rm = TRUE))
+  away_hits_total <- as.character(sum(as.numeric(linescore$away_hits), na.rm = TRUE))
+  home_hits_total <- as.character(sum(as.numeric(linescore$home_hits), na.rm = TRUE))
+  away_errors_total <- as.character(sum(as.numeric(linescore$away_errors), na.rm = TRUE))
+  home_errors_total <- as.character(sum(as.numeric(linescore$home_errors), na.rm = TRUE))
   
   box_score <- tibble(
     Inning = as.character(innings),
@@ -200,13 +206,13 @@ make_box_score <- function(linescore) {
     ),
     tibble(
       Inning = "H",
-      !!away_team := as.character(totals$away_hits),
-      !!home_team := as.character(totals$home_hits)
+      !!away_team := away_hits_total,
+      !!home_team := home_hits_total
     ),
     tibble(
       Inning = "E",
-      !!away_team := as.character(totals$away_errors),
-      !!home_team := as.character(totals$home_errors)
+      !!away_team := away_errors_total,
+      !!home_team := home_errors_total
     )
   )
   return(box_score)
