86676667249ba2d0561f67a083d2471045b5a08b
Anthony Giacalone <anthony.giacalone@csulb.edu>
2025-06-22 17:01:09 -0700

    add missing source for all_games_for_season

--- scripts/all_games_for_season.R
+++ scripts/all_games_for_season.R
@@ -1,6 +1,7 @@
 ### Get all the games for a specific season
 
 source ("R/season_games.R")
+source ("R/output_markdown.R")
 
 # Get the whole game schedule for the given season
 output_markdown(season_games(season), "output/all_season_games.md")
